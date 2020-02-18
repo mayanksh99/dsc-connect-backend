f19c026ce6c9de755665cc11299b0b43d791ed3f
mayanksh99 <mayankshakya992@gmail.com>
2020-02-19 00:10:51 +0530

    --update: all search added

--- controllers/data_controller.js
+++ controllers/data_controller.js
@@ -1,29 +1,21 @@
 module.exports.getData = async (req, res) => {
   let filter = {};
-  // if (req.query.location) {
-  //   filter = {
-  //     city: req.query.location,
-  //     state: req.query.location,
-  //     country: req.query.location
-  //   };
-  //   console.log(filter);
-  // }
   if (req.query.name) {
     filter = { name: req.query.name, ...filter };
-  }
-  if (req.query.domain) {
-    filter = { domains: req.query.domain, ...filter };
   }
   let data;
   let id = req.query.id;
   if (id) {
     data = await Dsc.findOne({ user: id });
     res.status(200).json({ message: "success", error: false, data });
-  } else if (req.query.location) {
+  } else if (req.query.location && req.query.domain) {
     let city = { city: req.query.location };
     let state = { state: req.query.location };
     let country = { country: req.query.location };
-    data = await Dsc.find(state || city || country).sort({
+    let domain = { domains: req.query.domain };
+    data = await Dsc.find({
+      $and: [{ $or: [city, state, country] }, { $or: [domain] }]
+    }).sort({
       createdAt: "desc"
     });
     res.status(200).json({ message: "success", error: false, data });
