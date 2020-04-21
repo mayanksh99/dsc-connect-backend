8aeb8d80c03536b0e0fe8eea2d649ab625698563
mayanksh99 <mayankshakya992@gmail.com>
2020-04-21 20:08:23 +0530

    --fix:email body

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-module.exports.generateCode = length => {
+module.exports.generateCode = (length) => {
   let chars = "abcdefghijklmnopqrstuvwxyz";
   let code = "";
   for (let i = 0; i < length; i++) {
@@ -14,13 +14,13 @@ module.exports.generateCode = length => {
 module.exports.register = async (req, res) => {
   let { name, email, password } = req.body;
   let user = await User.findOne({
-    email: { $regex: `^${email}$`, $options: "i" }
+    email: { $regex: `^${email}$`, $options: "i" },
   });
   if (user) {
     res.status(400).json({
       message: "Email already in use.",
       error: true,
-      data: null
+      data: null,
     });
   } else {
     try {
@@ -30,8 +30,8 @@ module.exports.register = async (req, res) => {
         service: "gmail",
         auth: {
           user: process.env.EMAIL, // generated ethereal user
-          pass: process.env.PASS // generated ethereal password
-        }
+          pass: process.env.PASS, // generated ethereal password
+        },
       });
 
       // send mail with defined transport object
@@ -39,25 +39,22 @@ module.exports.register = async (req, res) => {
         from: '"DSC CONNECT" <devsdsckiet@gmail.com>', // sender address
         to: Email, // list of receivers
         subject: "Verification Email", // Subject line
-        html: `Please click on this link to verify your account. http://localhost:5000/index/verifyEmail/${code}` // html body
+        html: `Please click on this link to verify your account. https://dsc-connect.netlify.app/verifyEmail/${code}`, // html body
       });
       let newUser = {
         name: String(name).trim(),
         email: String(email).trim(),
-        verifyToken: String(code)
+        verifyToken: String(code),
       };
       const salt = await bcrypt.genSalt(10);
       newUser["password"] = await bcrypt.hash(password, salt);
       user = await User.create(newUser);
       let token = user.generateAuthToken();
-      res
-        .status(200)
-        .header("x-auth-token", token)
-        .json({
-          message: "Successfully Registered",
-          error: false,
-          data: user
-        });
+      res.status(200).header("x-auth-token", token).json({
+        message: "Successfully Registered",
+        error: false,
+        data: user,
+      });
     } catch (error) {
       res
         .status(400)
@@ -69,7 +66,7 @@ module.exports.register = async (req, res) => {
 module.exports.login = async (req, res) => {
   let { email, password } = req.body;
   let user = await User.findOne({
-    email: { $regex: `^${email}$`, $options: "i" }
+    email: { $regex: `^${email}$`, $options: "i" },
   });
   if (user) {
     let validPassword = await bcrypt.compare(
@@ -80,24 +77,21 @@ module.exports.login = async (req, res) => {
       return res.status(403).json({
         message: "Invalid password",
         error: true,
-        data: null
+        data: null,
       });
     } else {
       let token = user.generateAuthToken();
-      res
-        .status(200)
-        .header("x-auth-token", token)
-        .json({
-          message: "Login Successful",
-          error: null,
-          data: user
-        });
+      res.status(200).header("x-auth-token", token).json({
+        message: "Login Successful",
+        error: null,
+        data: user,
+      });
     }
   } else {
     res.status(401).json({
       message: "Invalid User",
       error: true,
-      data: null
+      data: null,
     });
   }
 };
@@ -110,7 +104,7 @@ module.exports.profile = async (req, res) => {
     res.status(400).json({
       message: "No User found",
       error: true,
-      data: null
+      data: null,
     });
   }
 };
